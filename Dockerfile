7c903c5cf7985b6b59b5ba329773c73a67a8c527
rachidFMCG <130847876+Rachidbaiou@users.noreply.github.com>
2024-05-24 23:08:37 +0200

    commit

--- Dockerfile
+++ Dockerfile
@@ -7,5 +7,6 @@ COPY requirements.txt ./requirements.txt
 RUN pip install -r requirements.txt
 
 COPY app.py ./app.py
-
+EXPOSE 5000
 CMD ["python", "app.py"]
+
--- app.py
+++ app.py
@@ -1,5 +1,19 @@
-def hello_world():
-  print("Hello, World!")
+from flask import Flask, request, jsonify, render_template
 
-if __name__ == "__main__":
-  hello_world()
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/api/data', methods=['POST'])
+def process_data():
+    data = request.get_json()
+    response_data = {
+        "message": "Data received successfully",
+        "received_data": data
+    }
+    return jsonify(response_data)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
